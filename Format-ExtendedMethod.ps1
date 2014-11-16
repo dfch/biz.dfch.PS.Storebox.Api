72a05b0fe4712c65ff10dd8f30bc4687cbf8853a
Ronald Rink <ronald.rink@d-fens.net>
2014-11-16 07:44:17 +0100

    2014-11-16; rrink; CHG: end of cmdlet logging

--- Format-ExtendedMethod.ps1
+++ Format-ExtendedMethod.ps1
@@ -82,7 +82,7 @@ function Format-ExtendedMethod {
 	} # PROCESS
 	END {
 	$datEnd = [datetime]::Now;
-	Log-Debug -fn $fn -msg "RET. fReturn: [$fReturn]. Execution time: [$(($datEnd - $datBegin).TotalMilliseconds)]ms. Started: [$($datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz'))]." -fac 2;
+	Log-Debug -fn $fn -msg ("RET. fReturn: [{0}]. Execution time: [{1}]ms. Started: [{2}]." -f $fReturn, ($datEnd - $datBegin).TotalMilliseconds, $datBegin.ToString('yyyy-MM-dd HH:mm:ss.fffzzz')) -fac 2;
 	} # END
 } # Format-ExtendedMethod
 Set-Alias -Name Format-SBExtendedMethod -Value Format-ExtendedMethod;

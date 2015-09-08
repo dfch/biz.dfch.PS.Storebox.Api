f73dcad3523cac5731690f7eda18c6ba15e3ad24
Ronald Rink <ronald.rink@d-fens.net>
2015-09-08 09:22:18 +0200

    cleaned script, resolves #7

--- Set-DeviceTemplate.ps1
+++ Set-DeviceTemplate.ps1
@@ -2,25 +2,48 @@
 
 PARAM
 (
-	$PortalUri = "https://eni.ds98.swisscom.com"
+	# Specifies the Storebox portal to login
+	[ValidateNotNullOrEmpty()]
+	$PortalUri
 	,
-	$Username = "test-api"
+	# Specifies the username for login
+	[ValidateNotNullOrEmpty()]
+	$Username
 	,
+	# Specifies the password for login
+	[ValidateNotNullOrEmpty()]
 	$Password
 	,
-	$deviceTemplateName = "storebox_agent"
-	# ,
-	# $deviceName = "W2012-7"
+	# Specifies the name of the device to process
+	[ValidateNotNullOrEmpty()]
+	$deviceName
 	,
-	$deviceName = $ENV:COMPUTERNAME.ToLower()
+	# Specifies the name of the device template to use
+	$deviceTemplateName
 )
 
+# Parameter validation
+if([String]::IsNullOrWhiteSpace($PortalUri))
+{
+	throw ("PortalUri: Parameter validation FAILED. Value must not be null or empty.");
+}
+
+if([String]::IsNullOrWhiteSpace($Username))
+{
+	throw ("Username: Parameter validation FAILED. Value must not be null or empty.");
+}
+
+if([String]::IsNullOrWhiteSpace($Password))
+{
+	throw ("Password: Parameter validation FAILED. Value must not be null or empty.");
+}
 # Getting credential from some configuration file
-$cred = Get-Credential $Username;
+$EncryptedPassword = $Password | ConvertTo-SecureString -asPlainText -Force;
+$cred = New-Object System.Management.Automation.PSCredential($Username, $EncryptedPassword);
 
 # login to portal
 $svc = Enter-Ctera $PortalUri -Credential $cred -Global;
-if($null == $svc)
+if($null -eq $svc)
 {
 	throw ("Login to '{0}' with '{1}' FAILED." -f $PortalUri, $Username);
 }
@@ -29,16 +52,28 @@ if($null == $svc)
 $deviceTemplates = Invoke-CteraRestCall deviceTemplates | ConvertFrom-CteraObjList
 # ... and specific device template
 $deviceTemplate = $deviceTemplates | ? name -eq $deviceTemplateName;
-if($null == $deviceTemplate)
+if($null -eq $deviceTemplate)
 {
 	throw ("deviceTemplate '{0}' does not exist on '{1}'." -f $deviceTemplateName, $PortalUri);
 }
 
-# Getting 'our' template ...
-$device = Invoke-CteraRestCall ("devices/{0}" -f $deviceName) | ConvertFrom-CteraObj
-if($null == $device)
+# Getting 'our' device ...
+$deviceRaw = Invoke-CteraRestCall ("devices/{0}" -f $deviceName);
+if($null -eq $deviceRaw)
 {
 	throw ("device '{0}' does not exist on portal '{1}'." -f $deviceName, $PortalUri);
+}
+$device = $deviceRaw | ConvertFrom-CteraObj;
+if($null -eq $device)
+{
+	throw ("device '{0}' does not exist on portal '{1}'." -f $deviceName, $PortalUri);
+}
+
+# Check if template already matches target template name
+if($device.template -eq $deviceTemplate.baseObjectRef)
+{
+	Write-Warning ("device '{0}' is already set to deviceTemplate '{1}'. Skipping ..." -f $deviceName, $deviceTemplateName);
+	Exit;
 }
 
 # assigning deviceTemplate to device ...
@@ -48,7 +83,7 @@ $result = Update-CteraObj $device -Validate -Confirm:$false;
 
 # Verifying save operation and reloading device
 $deviceUpdated = Invoke-CteraRestCall ("devices/{0}" -f $deviceName) | ConvertFrom-CteraObj
-if($null == $deviceUpdated)
+if($null -eq $deviceUpdated)
 {
 	throw ("deviceUpdated '{0}' does not exist on portal '{1}'. Save operation seems to have FAILED." -f $deviceName, $PortalUri);
 }
